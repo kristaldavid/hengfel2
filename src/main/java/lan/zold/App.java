a7ab121514d963e190d966965c34cd5af58768b4
Kristál Dávid <kristaldavid@ktch.hu>
2025-02-06 22:00:05 +0100

    kész

---
+++ src/main/java/lan/zold/App.java
@@ -0,0 +1,47 @@
+/*
+* File: App.java
+* Author: Kristál Dávid 
+* Copyright: 2025, Kristál Dávid
+* Group: Szoft II-2-N
+* Date: 2025-02-06
+* Github: https://github.com/kristaldavid/
+* Licenc: MIT
+*/
+package lan.zold;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("mainScene"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/lan/zold/MainController.java
@@ -0,0 +1,42 @@
+/*
+* File: MainController.java
+* Author: Kristál Dávid 
+* Copyright: 2025, Kristál Dávid
+* Group: Szoft II-2-N
+* Date: 2025-02-06
+* Github: https://github.com/kristaldavid/
+* Licenc: MIT
+*/
+package lan.zold;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private TextField lengthField;
+
+    @FXML
+    private TextField radiusField;
+
+    @FXML
+    private TextField surfaceareaField;
+
+    @FXML
+    void onClickCalcButton(ActionEvent event) {
+        try {
+            double r = Double.parseDouble(radiusField.getText());
+            double l = Double.parseDouble(lengthField.getText());
+
+            double surfaceArea = 2 * Math.PI * r * (r + l);
+
+            surfaceareaField.setText(String.format("%.2f", surfaceArea));
+        } catch (NumberFormatException e) {
+            surfaceareaField.setText("Hibás bevitel!");
+        }
+    }
+
+
+}
